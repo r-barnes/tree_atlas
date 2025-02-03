628d0b3ce3b187b4fb4411bb7fd31a7b324c745f
Richard Barnes <rbarnes@umn.edu>
2025-02-03 15:47:22 -0800

    Comment code

--- downloader.py
+++ downloader.py
@@ -15,20 +15,27 @@ OUT_DIR.mkdir(exist_ok=True)
 
 def save_page(url: str, base: str, load_wait: int = 3, save_wait: int = 3) -> None:
     print(f"Archiving {url}")
+    # Open page in new tab
     subprocess.run(["google-chrome-stable", url], check=True)
     time.sleep(load_wait)
+    # Attempt to save the page until we succeedSS
     while True:
+        # Does nothing if page not loaded
         subprocess.run(["xdotool", "key", "Control_L+s"], check=True)
         time.sleep(1)
+        # Does nothing if page not loaded
         subprocess.run(["xdotool", "key", "Enter"], check=True)
         time.sleep(save_wait)
         tree_number = Path(url).name
         try:
+            # Fails if the page didn't save because src dirctory will be missing
             shutil.move(Path("/z/downloads"), OUT_DIR / f"{base}_{tree_number}")
         except FileNotFoundError:
             continue
         break
+    # Close tab we opened to avoid exhausting resources
     subprocess.run(["xdotool", "key", "Control_L+w"], check=True)
+    # Display progress by showing terminal window
     subprocess.run(["xdotool", "key", "F12"], check=True)
     time.sleep(0.5)
     subprocess.run(["xdotool", "key", "F12"], check=True)
